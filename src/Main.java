169d35faf6e7299b6146175d8d0f00174369ce20
LakshithaMaduranga <155385829+LakshithaMaduranga@users.noreply.github.com>
2024-06-29 22:23:55 +0530

    First commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void method(){
+        int i=0;
+        do{
+            System.out.println(i);
+            i++;
+        }while (i<=5);
+    }
+    public static void main(String[] args) {
+        method();
+    }
+}
